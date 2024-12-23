f2f2fe224bb1c4855e646e5c579d4b78f66c2643
o1egnazarov <noleg867@gmail.com>
2024-12-23 13:34:37 +0600

    first commit

---
+++ src/main/java/ru/noleg/crmmm/CrmMmApplication.java
@@ -0,0 +1,13 @@
+package ru.noleg.crmmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmMmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmMmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/noleg/crmmm/CrmMmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.noleg.crmmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmMmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
